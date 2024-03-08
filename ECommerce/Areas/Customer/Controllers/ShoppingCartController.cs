552ca60da4915b192b4da7d2a038cef2cd7baeb3
Nihar Patel <patel.nihar.chandresh@gmail.com>
2024-03-07 16:24:57 -0800

    order detail and order header entered into databas

--- ECommerce/Areas/Customer/Controllers/ShoppingCartController.cs
+++ ECommerce/Areas/Customer/Controllers/ShoppingCartController.cs
@@ -1,6 +1,7 @@
 ﻿using ECommerce.DataAccess.Repository.IRepository;
 using ECommerce.Models.Models;
 using ECommerce.Models.ViewModels;
+using ECommerce.Utility;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
@@ -14,9 +15,16 @@ namespace ECommerce.Areas.Customer.Controllers
         private IShoppingCartRepository repo;
         private IProductRepository productRepository;
         private IApplicationUserRepository applicationUserRepository;
-        //public ShoppingCartVM shoppingCartVM { get; set; }
-        public ShoppingCartController(IShoppingCartRepository sc, IProductRepository productRepository, IApplicationUserRepository userRepository)
+        [BindProperty]
+        public ShoppingCartVM scVM { get; set; }
+        private IOrderHeaderRepository orderHeaderRepository { get; set; }
+        private IOrderDetailRepository orderDetailRepository { get; set; }
+        
+        public ShoppingCartController(IShoppingCartRepository sc, IProductRepository productRepository, IApplicationUserRepository userRepository,
+            IOrderHeaderRepository orderHeader, IOrderDetailRepository orderDetail)
         {
+            this.orderHeaderRepository = orderHeader;
+            this.orderDetailRepository = orderDetail;
             this.applicationUserRepository = userRepository;
             this.productRepository = productRepository;
             repo = sc;
@@ -28,7 +36,7 @@ namespace ECommerce.Areas.Customer.Controllers
             var claimsIdentity = (ClaimsIdentity)User.Identity;
             var uId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
 
-            ShoppingCartVM scVM = new()
+            scVM = new()
             {
                 ShoppingCartList = repo.GetAll(u => u.UserId == uId, includeProperties: "Product"),
                 OrderHeader = new()
@@ -82,7 +90,7 @@ namespace ECommerce.Areas.Customer.Controllers
 
             ApplicationUser applicationUser = applicationUserRepository.Get(u => u.Id == uId);
 
-            ShoppingCartVM scVM = new()
+            scVM = new()
             {
                 ShoppingCartList = repo.GetAll(u => u.UserId == uId, includeProperties: "Product"),
                 OrderHeader = new()
@@ -102,7 +110,56 @@ namespace ECommerce.Areas.Customer.Controllers
         }
 
 
-        private double CalculateOrderTotal(IEnumerable<ShoppingCart> shoppingCart)
+        [HttpPost]
+        [ActionName("Summary")]
+		public IActionResult SummaryPOST()
+		{
+
+			var claimsIdentity = (ClaimsIdentity)User.Identity;
+			var uId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+
+            scVM.ShoppingCartList = repo.GetAll(u => u.UserId == uId, includeProperties: "Product");
+
+            scVM.OrderHeader.ApplicationUserId = uId;
+
+            scVM.OrderHeader.ApplicationUser = applicationUserRepository.Get(u => u.Id == uId);
+
+            scVM.OrderHeader.OrderDate = DateTime.Now;
+
+            scVM.OrderHeader.OrderTotal = CalculateOrderTotal(scVM.ShoppingCartList);
+
+            if(scVM.OrderHeader.ApplicationUser.CompanyId.GetValueOrDefault() == 0)
+            {
+                scVM.OrderHeader.OrderStatus = SD.Status_Pending;
+                scVM.OrderHeader.PaymentStatus = SD.Payment_Status_Pending;
+
+            }else
+            {
+				scVM.OrderHeader.OrderStatus = SD.Status_Approved;
+				scVM.OrderHeader.PaymentStatus = SD.Payment_Status_Delayed_Payment;
+			}
+            orderHeaderRepository.Add(scVM.OrderHeader);
+            orderHeaderRepository.Save();
+
+            foreach(var item in scVM.ShoppingCartList)
+            {
+                OrderDetail orderDetail = new()
+                {
+                    ProductId = item.ProductId,
+                    OrderHeaderId = scVM.OrderHeader.Id,
+                    Price = item.Product.Price,
+                    Count = item.Count
+                };
+                orderDetailRepository.Add(orderDetail);
+                orderDetailRepository.Save();
+            }
+
+			return View(scVM);
+		}
+
+
+		private double CalculateOrderTotal(IEnumerable<ShoppingCart> shoppingCart)
         {
             double total = 0;
             if(shoppingCart != null)
